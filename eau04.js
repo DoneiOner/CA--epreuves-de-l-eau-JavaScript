c14c132bdfefecff61aad833949ff1c9c7ce9f5f
donei <donei.dev@gmail.com>
2022-01-19 12:43:38 +0100

    enoncé

---
+++ eau04.js
@@ -0,0 +1,12 @@
+/*     Prochain nombre premier
+
+Créez un programme qui affiche le premier nombre premier supérieur au nombre donné en argument.
+
+
+Exemples d’utilisation :
+$> python exo.py 14
+17
+$>
+
+Afficher -1 si le paramètre est négatif ou mauvais.
+*/
