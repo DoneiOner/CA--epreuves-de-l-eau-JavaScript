a8c967f8640ee9e34fc5ab6f275339b07595a4d8
donei <donei.dev@gmail.com>
2022-01-20 12:06:57 +0100

    exo 02

--- eau02.js
+++ eau02.js
@@ -18,4 +18,27 @@ $> python exo.py “Bonjour 36”
 Bonjour 36
 
 Afficher error et quitter le programme en cas de problèmes d’arguments.
-/*
+*/
+
+
+// gestion des erreurs
+if (process.argv.length < 3)  {
+    console.log("Erreur.");
+    return
+}
+
+// parsing
+tableau = [];
+for (i=2; i<process.argv.length; i++) {
+    tableau.push(process.argv[i]);
+}
+//console.log(tableau);
+//console.log(tableau.length);
+// resolution
+for (let index = tableau.length -1; index >= 0 ; index--) {
+   
+   let word = tableau[index];
+   //console.log(word);
+   process.stdout.write(word.toString() + " ");    
+}
+console.log('');
