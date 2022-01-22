99817b7d3878ee67bbc8b6848b4959f2485577d2
donei <donei.dev@gmail.com>
2022-01-22 17:38:05 +0100

    exo 11

--- eau11.js
+++ eau11.js
@@ -1,6 +1,7 @@
 /*     Différence minimum absolue
 
-Créez un programme qui affiche la différence minimum absolue entre deux éléments d’un tableau constitué uniquement de nombres. Nombres négatifs acceptés.
+Créez un programme qui affiche la différence minimum absolue entre deux éléments d’un tableau constitué uniquement de nombres.
+Nombres négatifs acceptés.
 
 
 Exemples d’utilisation :
@@ -17,3 +18,72 @@ $> python exo.py -8 -6 4
 Afficher error et quitter le programme en cas de problèmes d’arguments.
 
 */
+
+// PSEUDO CODE :
+// REMPLIR UN TABLEAU DES NOMBRES
+//MINDIFF = 0
+// FONCTION POUR PARCOURIR MON TABLEAU => ALIMENTER MINDIFF D'APRES LE NOMBRE FOURNI EN ARGUMENT A LA FONCTION
+// BOUCLE POUR APPELER LA FONCTION DEPUIS CHAQUE INDEX DE MON TABLEAU
+
+
+
+// GESTION D'ERREURS : 
+for (let i = 2; i < process.argv.length; i++) {
+    let saisie = process.argv[i];
+    if ((isNaN(saisie))) {
+        console.log("Erreur.");
+        process.exit(0);
+    }   
+}
+if (process.argv.length <= 3 ) {
+    console.log("Erreur.");
+        process.exit(0);
+}
+
+
+
+// PARSING : 
+let array = [];
+
+for (let i = 2; i < process.argv.length; i++) {
+    array.push(parseInt(process.argv[i]));     
+}
+
+let minDiff = Math.abs(array[0] - array[1]);
+
+
+
+
+
+// FONCTIONS :
+function differenceBetween(n) {
+    for (let i = n + 1 ; i < array.length; i++) {
+
+        let diff = Math.abs((array[n]) - (array[i]));
+        //console.log(diff);
+        if (diff < minDiff) {
+            minDiff = diff;
+        }      
+    }
+    return minDiff;
+}
+
+
+
+// RESOLUTION : 
+for (let n = 0; n < array.length; n++ ) {
+    //console.log(array[n]);
+    differenceBetween(n);
+    
+};
+
+console.log(minDiff);
+
+
+
+
+
+
+
+
+
