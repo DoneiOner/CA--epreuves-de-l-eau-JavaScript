0a0aebf6f80eb5a4395b054f1fc975cd91a299c9
donei <donei.dev@gmail.com>
2022-01-22 15:20:46 +0100

    exo 08

--- eau08.js
+++ eau08.js
@@ -17,3 +17,32 @@ false
 Afficher error et quitter le programme en cas de problèmes d’arguments.
 
 */
+
+// PSEUDO CODE :
+
+// REMPLIR UN TABLEAU AVEC CHAQUE CARACTERE EN ARGUMENT
+
+// PARCOURIR MON TABLEAU, COMPARER (ISNAN), RETOURNER FALSE SI !ISNAN SINON SI BOUCLE COMPLETE : AFFICHER TRUE
+
+
+// GESTION D'ERREURS : 
+if (process.argv[2] == null || (process.argv.length > 3)) {
+    console.log('Erreur.');
+    process.exit(0);
+}
+// PARSING :
+const saisie = process.argv[2];
+let tableau = [];
+for (let i = 0; i < saisie.length; i++) {
+    tableau.push(saisie[i]);    
+}
+//console.log(tableau);
+
+// RESOLUTION : 
+for (let i = 0; i < tableau.length; i++) {
+    if (isNaN(tableau[i])) {
+        console.log ("false");
+        process.exit(0);
+    }
+}
+console.log("true");
