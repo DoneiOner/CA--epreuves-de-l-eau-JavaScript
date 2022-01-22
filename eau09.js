dc25d8e493a2ff6b9859445deca177a85a0b7785
donei <donei.dev@gmail.com>
2022-01-22 15:53:36 +0100

    exo 09

--- eau09.js
+++ eau09.js
@@ -17,3 +17,32 @@ error
 Afficher error et quitter le programme en cas de problèmes d’arguments.
 
 */
+
+//  PSEUDO CODE : 
+
+// PARSER MIN ET MAX
+
+// FAIRE UNE BOUCLE ENTRE MIN ET MAX
+// AFFICHER CHAQUE NOMBRE DANS L'INTERVALLE, SAUF LE MAX 
+
+
+// GESTION D'ERREURS : 
+if (process.argv[2] == null || (isNaN(process.argv[2])) || (isNaN(process.argv[3])) || (process.argv.length > 4)) {
+    console.log('Erreur.');
+    process.exit(0);
+}
+//console.log(process.argv[2]);
+// PARSING:
+let min = process.argv[2];
+let max = process.argv[3];
+
+if ( process.argv[2] > process.argv[3] ) {
+     min = process.argv[3];
+     max = process.argv[2];
+}
+
+// RESOLUTION :
+for (let i = min; i < max; i++) {
+    process.stdout.write(i + " ");    
+}
+console.log(" ");
