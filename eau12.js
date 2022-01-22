63ed4b764600ad8d63956acb9e3deef074d1615d
donei <donei.dev@gmail.com>
2022-01-22 23:52:36 +0100

    exo 12

--- eau12.js
+++ eau12.js
@@ -23,3 +23,50 @@ Afficher error et quitter le programme en cas de problèmes d’arguments.
 Wikipedia vous présentera une belle description de cet algorithme de tri.
 
 */
+
+// PSEUDO CODE
+// UNE BOUCLE POUR VERIFIER SI INDEX N < INDEX N+1
+//                         SINON, PERMUTER ET PERMUTATION = TRUE
+// AU DEBUT DE LA BOUCLE : PERMUTATION = FALSE
+// TANT QUE PERMUTATION = TRUE EN FIN DE BOUCLE, LA BOUCLE CONTINUE
+
+
+// GESTION D'ERREURS
+
+
+// PARSING
+let array = [];
+let new_array = [];
+let permutation = true;
+
+for (let i = 2; i < process.argv.length; i++) {
+	array.push(process.argv[i]);	
+}
+console.log(array);
+
+// FONCTIONS
+function my_bubble_sort(array) {
+	permutation = false ; 
+	for (let i = 0; i < array.length; i++) {
+		if ( array[i] > array[i+1] ) {
+			permutation = true
+			let a = array[i];
+			let b = array[i+1];
+						array.splice(i+1, 1, a);
+			array.splice(i, 1, b);
+		} 
+	}
+	return (array, permutation);
+}
+
+
+// RESOLUTION
+while (permutation === true) {
+my_bubble_sort(array); 
+}
+
+
+// AFFICHAGE
+for (let i = 0; i < array.length; i++) {
+	process.stdout.write(array[i] + " ");	
+}
