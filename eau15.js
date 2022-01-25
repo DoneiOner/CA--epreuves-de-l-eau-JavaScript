3be71b082e264db40cddba8f2aa32183ce39ff48
donei <donei.dev@gmail.com>
2022-01-25 14:25:16 +0100

    exo 15

--- eau15.js
+++ eau15.js
@@ -11,3 +11,14 @@ $>
 Note : [] est à remplacer par un adjectif de votre choix (facile ?)
 
 */
+
+
+
+// AFFICHAGE
+console.log();
+console.log(" J'ai terminé l'Epreuve de l'Eau et c'etait cool de se faire des noeuds au cerveau seulement de temps en temps (mais longtemps)");
+console.log();
+console.log(" et je pense que j'ai compris comment faire pour la suite : bosser vraiment le pseudo code avant de coder quoique ce soit");
+console.log();
+console.log(" deter' pour la suite");
+console.log();
