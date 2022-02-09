c11cef0a29cec736e97bdd61b9751667c9a45475
donei <donei.dev@gmail.com>
2022-02-09 12:44:25 +0100

    fix

--- eau04.js
+++ eau04.js
@@ -19,10 +19,10 @@ let args = process.argv
 // GESTION D'ERREURS
 // Condition qui gère l'absence d'argument ou un argument en trop ou un argument non numerique  ou un argument négatif
 if(args[2] === undefined || process.argv.length > 3  ) {
-    console.log("Entrez un seul nombre !");
+    console.log("Entrez un nombre et un seul !");
      process.exit(0)
 } else if (isNaN(parseInt(args[2])) ||  args[2] < 0) {
-    console.log("Entrez un entier POSITIF !")
+    console.log("Entrez un entier positif !")
     process.exit(0)
 }
 
@@ -34,19 +34,36 @@ let N = parseInt(args[2])
 
 // FONCTIONS
 
-function nbrPremier(N) {    
-    for(var i = 2; i <= Math.sqrt(N); i++) {    
-        if(N%i === 0) {
-            return false ;
-        }
-        
-        return N > 1 ;
-    } 
-}
+function isPrime(nombre_a_tester) {
+    if ((nombre_a_tester < 2)) {
+        return false;
+    }
+    if (nombre_a_tester == 2 ) {
+        console.log('Oui, 2 est un nombre premier');
+        process.exit(0);
+    }
+    if(nombre_a_tester%2 === 0) {
+        return false;
+    }
+
+    var racine = Math.sqrt(nombre_a_tester);
+
+    if(racine == parseInt(racine)) {
+        return false;
+    }
+
+    for ( let i = 3; i<=racine; i+=2) {        
+        if(nombre_a_tester%i === 0) {            
+            return false;
+        }        
+    }
+
+    return N;
+    }
 
 
 function findPremier(N) {
-    while (nbrPremier(N) === false) {
+    while (isPrime(N) === false) {
         N++
     }
     console.log("Le nombre premier supérieur est "+N);}
